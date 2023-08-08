e676bf0856bc5ff19e2b5d8779e5940303c226a0
Richard Muchira <rwachira.rw@gmail.com>
2023-08-08 16:07:04 +0300

    Added test_base_model

--- models/base_model.py
+++ models/base_model.py
@@ -28,10 +28,16 @@ class BaseModel:
         Returns:
             BaseModel instance
         """
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
