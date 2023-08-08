7e81ff38db91e3d53848b12b680c4d73b34933c3
Eric Ubaga <ubaga91@gmail.com>
2023-08-08 16:50:21 +0300

    Added the file file_storage.py: implements the FileStorage Class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+Defines class FileStorage
+Serializes instances to a JSON file and deserializes JSON file to instances
+"""
+
+import json
+import os
+
+
+class FileStorage:
+    """
+    Class to perform Serialization and Deserialization to JSON files
+
+    Attributes:
+        __file_path
+        __objects
+    Methods:
+        all()
+        new()
+        save()
+        reload()
+    """
+
+    __file_path = "airbnb_file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Method to ectract the stored objects
+
+        Returns:
+            Attribute __objects; the dictionary of all stored objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Store created objects into the sictioanry
+
+        Args:
+            obj: object class type to be stored
+        """
+
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        Serialize the __objects to the JSON file in path __file_path
+
+        __objects dictionary contains instances of custom classes. We cannot use directly json.dump().
+        We have to call the to_dict() method on each object to convert it into a dictionary representation
+        before serializing it using json.dump().
+        """
+
+        serial_objects = {}
+
+        for k, v in FileStorage.__objects.items():
+            serial_objects[k] = v.to_dict()
+        
+        with open(FileStorage.__file_path, mode='w', encoding="utf-8") as f:
+            json.dump(serial_objects, f)
+
+    def reload(self):
+        """
+        Deserialize the JSON file to __objects.
+        If __file_path does not exit, do nothing.
+        """
+
+        from models.base_model import BaseModel
+        class_list = [BaseModel]
+
+        try:
+            with open(FileStorage.__file_path, mode='r') as file:
+                obj_data = json.load(file)
+                
+                for val in obj_data.values():
+                    class_name = obj_data["__class__"]
+                    if class_name in class_list:
+                        self.new(class_name(**val))
+                    """
+                    class_module = __import__("models." + class_name,
+                                              fromlist=[class_name])
+                    class_obj = getattr(class_module, class_name)
+                    self.new(class_obj(**val))
+                    """
+        except FileNotFoundError:
+            pass
+
