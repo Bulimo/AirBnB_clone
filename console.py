4c585f251fd19f0570d16c71098eb7e6b7bfc34e
Richard Muchira <rwachira.rw@gmail.com>
2023-08-11 21:34:17 +0300

    Updated the Filestorage class

--- console.py
+++ console.py
@@ -5,6 +5,14 @@ Defines class HBNBCommand()
 """
 import cmd
 import readline
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
