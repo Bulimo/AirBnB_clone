adc72358e533d5f2a3b658042d3b49cd5397fc51
Richard Muchira <rwachira.rw@gmail.com>
2023-08-13 23:41:23 +0300

    Updated the default method to upddate() input

--- console.py
+++ console.py
@@ -279,10 +279,14 @@ class HBNBCommand(cmd.Cmd):
         elif args[1].startswith("update(") and args[1].endswith(")"):
             update_args = args[1][7:-2].split(', ')
             instance_id = update_args[0]
-            dict_repr = update_args[1].strip()[1:-1]
-            update_dict = eval(dict_repr)
-            update_command = "{} {} {}".format(class_name,
-                                               instance_id, update_dict)
+            attribute_name = update_args[1]
+            attribute_value = update_args[2][1:-1]
+            update_command = (
+                "{} {} {} {}"
+                .format(class_name, instance_id,
+                        attribute_name, attribute_value)
+            )
+
             HBNBCommand.do_update(self, update_command)
         else:
             print("*** Unknown syntax: {}".format(arg))
