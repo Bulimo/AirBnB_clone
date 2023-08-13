a6ea86a872de01f22aef98ed735911be6b80e792
Richard Muchira <rwachira.rw@gmail.com>
2023-08-13 20:04:40 +0300

    Reverted the commits

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Parent to all classes
 
 from datetime import datetime
 from models import storage
-from uuid import uuid4
+import uuid
 
 
 class BaseModel:
@@ -34,11 +34,11 @@ class BaseModel:
         if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    v = datetime.fromisoformat(v)
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 if k != "__class__":
                     setattr(self, k, v)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
@@ -51,7 +51,7 @@ class BaseModel:
             Str: string representation of the object
         """
 
-        return "[{}] ({}) {}".format(type(self).__name__,
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
